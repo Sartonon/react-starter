d865101e3635f3636727eba00ddf6e8d27de29bd
Santeri Rusila <santtu_1993@hotmail.com>
2019-09-21 15:22:32 +0300

    lint test

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { hot } from 'react-hot-loader/root'
+
+const Warning = React.lazy(() => import('./Warning'))
+
+const a = '2'
+
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  render() {
+    console.log('Moi')
+    return (
+      <div>
+        <h1>Hello world!!</h1>
+        {this.state.count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : (
+          ''
+        )}
+        <h2>Count: {this.state.count}</h2>
+
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+      </div>
+    )
+  }
+}
+
+export default hot(App)
